bb4b23627437860b466c6b98468b053a7b416da9
unknown <rod_rahmjoo@yahoo.com>
2020-03-27 15:29:05 -0400

    Updates

---
+++ precache-manifest.3515ec1a058281f7c3dd4abd639c0e4c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c20ca0f0730169cdddc6458c07f65681",
+    "url": "/productive-youtube/index.html"
+  },
+  {
+    "revision": "a0a153c0aba78eb4ca6f",
+    "url": "/productive-youtube/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "1e79a4719f2b090d93ec",
+    "url": "/productive-youtube/static/css/main.86971cc4.chunk.css"
+  },
+  {
+    "revision": "a0a153c0aba78eb4ca6f",
+    "url": "/productive-youtube/static/js/2.f2e40d7b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/productive-youtube/static/js/2.f2e40d7b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e79a4719f2b090d93ec",
+    "url": "/productive-youtube/static/js/main.8625e7fd.chunk.js"
+  },
+  {
+    "revision": "7f551910f7f843c3108a",
+    "url": "/productive-youtube/static/js/runtime-main.0cf8c697.js"
+  }
+]);
